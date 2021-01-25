d29319a9c4d24e0c77f0a67671b0147ea5ad8c8e
amanjotsinghdhunna <amanjotsingh260@gmail.com>
2021-01-25 19:37:28 +0530

    added basic questions file

---
+++ otherFiles/script.js
@@ -0,0 +1,57 @@
+
+console.log('a')
+console.log('b')
+
+for (let i = 0; i < 5; i++) { console.log('i') } // web
+// i i i i i
+console.log('f')
+
+const abc = new Promise((resolve, reject) => {resolve('d')});
+setTimeout(()=> console.log('c'), 0)
+abc.then((a) => console.log(a));
+
+setTimeout(()=> console.log('e'), 2000)
+for (let i = 0; i < 5; i++){setTimeout(()=> console.log('g + ', i), 1000)}
+
+
+// final ans a , b ,5 x i,  f, c ,d , g + (0 to 4), e 
+
+var 
+const 
+let ans
+{
+
+
+}
+
+console,log(x);
+// let x =5;
+var  x =5; 
+
+// class and function
+
+useEffect (( ) => { return () => { event unresitrer, unsuscibe} }, [depenc]) 
+
+for() {
+
+}
+
+for() {
+    for
+}
+// < button onClick=((e) => { import('./action').then((action) => action.callXHR(e) )})> 
+0(n)
+// Program to check if a subarray of two with 0 sum 
+let  arr = [2, - 2 ,4, 0, 1, 1, 2, -2, 3, 0, 9, 4, -5];
+
+console.log();
+/* const result = arr.reduce((acc, curr) => {
+    if(curr > 0 && arr.includes(-curr)) {
+        
+    }
+ } , []); */
+        
+
+
+console.log(result)
+// neha.agarwal@go-mmt.com
